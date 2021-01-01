4f9c56320b1607a3fa04dd7a3a6ed5f3abc42216
Michael <michael.zemler@hotmail.com>
2021-01-01 22:44:44 +0100

    formatting

--- performance.py
+++ performance.py
@@ -49,8 +49,6 @@ class PerformanceViewer(object):
         plt.ylabel('F1 Score')
         plt.legend()
         plt.show()
-
-        self.resetHistory()
    
     # same as evalModelTrainData, but plots for each class(b_s, i_s, o, overall) 
     # instead of metric (precision,recall,f1)
@@ -59,12 +57,11 @@ class PerformanceViewer(object):
             plt.plot(self.plot_epoch, self.plot_precision_class[i], label = "Precision")
             plt.plot(self.plot_epoch, self.plot_recall_class[i], label = "Recall")
             plt.plot(self.plot_epoch, self.plot_f1_class[i], label = "F1")
-            plt.title("Class "+convertIdToClass(i))
+            plt.title("Class "+self.convertIdToClass(i))
             plt.xlabel('Epochs')
             plt.ylabel('Performance')
             plt.legend()
             plt.show()
-        self.resetHistory()
 
     def convertIdToClass(self, id):
         if id == 0:
@@ -124,32 +121,23 @@ class PerformanceViewer(object):
         precision_per_class = np.zeros((vw.n_tags,))
         recall_per_class = np.zeros((vw.n_tags,))
         for i in range(vw.n_tags):
-
-            print("check gt 0")
-            print(conf_matrix.values[i,i])
-            print("")
             if conf_matrix.values[i,i] > 0:
-                print("CLASS = "+str(i))
+                print("class: "+self.convertIdToClass(i))
                 precision_per_class[i] = conf_matrix.values[i,i]/sum(conf_matrix.values[:,i])
-                print("precision")
-                print(precision_per_class[i])
+                print("precision: "+"{:.3f}".format(precision_per_class[i]))
                 recall_per_class[i] = conf_matrix.values[i,i]/sum(conf_matrix.values[i,:])
-                print("recall")
-                print(recall_per_class[i])
-                print("f1")
+                print("recall: "+"{:.3f}".format(recall_per_class[i]))
                 f1_class = 2*(precision_per_class[i]*recall_per_class[i])/(precision_per_class[i]+recall_per_class[i])
-                print(f1_class)
+                print("f1 "+"{:.3f}".format(f1_class))
             else:
-                print("no (0) precision or recall for class "+str(i))
+                print("no (None / NULL) precision or recall for class "+self.convertIdToClass(i))
         precision = np.mean(precision_per_class)
         recall = np.mean(recall_per_class)
         f1 = 2*(precision*recall)/(precision+recall)
         print()
-        print('Precision: '+str(precision))
-        print('Recall: '+str(recall))
-        print('F1-measure: '+str(f1))
-
-        print("\n")
+        print("Precision "+"{:.3f}".format(precision))
+        print("Recall "+"{:.3f}".format(recall))
+        print("F1-measure "+"{:.3f}".format(f1))
         
 #callback for checking performance after each epoch
 class TrainingEval(tf.keras.callbacks.Callback):
@@ -170,50 +158,49 @@ class TrainingEval(tf.keras.callbacks.Callback):
         self.performance = performance
     # This function is called at the end of each epoch
     def on_epoch_end(self, epoch, logs={}):
-        #define data accuracy with modulo 2 = high, 10 = low
-        if epoch % 2 == 1:
-            self.performance.plot_epoch.append(epoch)
-            ## evaluation
-            # predict labels of test data
-            y_test_pred_prob = self.model.predict(self.x_test)
-            y_test_pred_sparse = y_test_pred_prob.argmax(axis=-1)
-            y_test_pred = to_categorical(np.array(y_test_pred_sparse), num_classes=self.vw.n_tags)
+        # +1 because epochs start at 0
+        self.performance.plot_epoch.append(epoch+1)
+        ## evaluation
+        # predict labels of test data
+        y_test_pred_prob = self.model.predict(self.x_test)
+        y_test_pred_sparse = y_test_pred_prob.argmax(axis=-1)
+        y_test_pred = to_categorical(np.array(y_test_pred_sparse), num_classes=self.vw.n_tags)
 
-            # compute confusion matrix
-            conf_matrix = np.zeros((self.vw.n_tags, self.vw.n_tags))
-            for i,tokens in enumerate(self.ds.test_tokens):
-                for j,_ in enumerate(tokens):
-                    class_true = self.y_test[i,j].argmax()
-                    class_pred = y_test_pred[i,j].argmax()
-                    conf_matrix[class_true,class_pred] += 1
-            names_rows = list(s+'_true' for s in self.vw.labelclass_to_id.keys())
-            names_columns = list(s+'_pred' for s in self.vw.labelclass_to_id.keys())
-            conf_matrix = pd.DataFrame(data=conf_matrix,index=names_rows,columns=names_columns)
-            
-            # compute final evaluation measures
-            precision_per_class = np.zeros((self.vw.n_tags,))
-            recall_per_class = np.zeros((self.vw.n_tags,))
-            
-            for i in range(self.vw.n_tags):
-                if conf_matrix.values[i,i] > 0:
+        # compute confusion matrix
+        conf_matrix = np.zeros((self.vw.n_tags, self.vw.n_tags))
+        for i,tokens in enumerate(self.ds.test_tokens):
+            for j,_ in enumerate(tokens):
+                class_true = self.y_test[i,j].argmax()
+                class_pred = y_test_pred[i,j].argmax()
+                conf_matrix[class_true,class_pred] += 1
+        names_rows = list(s+'_true' for s in self.vw.labelclass_to_id.keys())
+        names_columns = list(s+'_pred' for s in self.vw.labelclass_to_id.keys())
+        conf_matrix = pd.DataFrame(data=conf_matrix,index=names_rows,columns=names_columns)
 
-                    precision_per_class[i] = conf_matrix.values[i,i]/sum(conf_matrix.values[:,i])
-    
-                    recall_per_class[i] = conf_matrix.values[i,i]/sum(conf_matrix.values[i,:])
+        # compute final evaluation measures
+        precision_per_class = np.zeros((self.vw.n_tags,))
+        recall_per_class = np.zeros((self.vw.n_tags,))
 
-                    f1_per_class = 2*(precision_per_class[i]*recall_per_class[i])/(precision_per_class[i]+recall_per_class[i])
-                    self.performance.plot_precision_class[i].append(precision_per_class[i])
-                    self.performance.plot_recall_class[i].append(recall_per_class[i])
-                    self.performance.plot_f1_class[i].append(f1_per_class)
-                else:
-                    self.performance.plot_precision_class[i].append(0)
-                    self.performance.plot_recall_class[i].append(0)
-                    self.performance.plot_f1_class[i].append(0)
-            precision = np.mean(precision_per_class)
-            recall = np.mean(recall_per_class)
-            f1 = 2*(precision*recall)/(precision+recall)
-            self.performance.plot_precision_class[self.vw.n_tags].append(precision)
-            self.performance.plot_recall_class[self.vw.n_tags].append(recall)
-            self.performance.plot_f1_class[self.vw.n_tags].append(f1)
-            #this eval only works, if we use validation_split = 0.2 as additional parameter for model.fit()
+        for i in range(self.vw.n_tags):
+            if conf_matrix.values[i,i] > 0:
+
+                precision_per_class[i] = conf_matrix.values[i,i]/sum(conf_matrix.values[:,i])
+
+                recall_per_class[i] = conf_matrix.values[i,i]/sum(conf_matrix.values[i,:])
+
+                f1_per_class = 2*(precision_per_class[i]*recall_per_class[i])/(precision_per_class[i]+recall_per_class[i])
+                self.performance.plot_precision_class[i].append(precision_per_class[i])
+                self.performance.plot_recall_class[i].append(recall_per_class[i])
+                self.performance.plot_f1_class[i].append(f1_per_class)
+            else:
+                self.performance.plot_precision_class[i].append(0)
+                self.performance.plot_recall_class[i].append(0)
+                self.performance.plot_f1_class[i].append(0)
+        precision = np.mean(precision_per_class)
+        recall = np.mean(recall_per_class)
+        f1 = 2*(precision*recall)/(precision+recall)
+        self.performance.plot_precision_class[self.vw.n_tags].append(precision)
+        self.performance.plot_recall_class[self.vw.n_tags].append(recall)
+        self.performance.plot_f1_class[self.vw.n_tags].append(f1)
+        #this eval only works, if we use validation_split = 0.2 as additional parameter for model.fit()
 
